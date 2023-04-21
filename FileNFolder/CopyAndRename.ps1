b25621821fa5f2aa09a072721c295926c0c43aad
Siken Dongol <sikenman@yahoo.com>
2023-04-21 17:10:37 -0500

    Copy and Rename PS

---
+++ FileNFolder/CopyAndRename.ps1
@@ -0,0 +1,10 @@
+# @Author: Siken M. Dongol
+# @Purpose: this ps has been made to copy daily bing wallpaper
+#   from Bing folder to folder of my choice and rename the file with the current date
+#   this ps will be called via Windows task Scheduler daily to copy bing wp
+# Note: Bing Wallpaper changes daily
+
+$date = Get-Date -Format "MM-dd-yyyy"
+$source = "C:\Users\SikenD\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper"
+$destination = "C:\Users\SikenD\Desktop\Bing Daily\bing-wp-$date.jpg"
+Copy-Item $source $destination
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MyPowerShell
 Some of my useful PowerShell Scripts
+---
+### CopyAndRename.ps1
+- Copy a file from one folder to another and renames it.
+- Can be used with Windows Task Scheduler to run it daily or any other time duration.
